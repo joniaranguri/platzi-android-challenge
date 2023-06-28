583dd5db305e09f4b7eba3f18ac29ff3653db567
Jonathan Aranguri <arangurim@gmail.com>
2023-06-28 17:06:40 -0300

    fix: Reusing RemoteModule from core lib

--- features/books/src/main/java/com/joniaranguri/platzi/android/books/list/data/local/di/LocalModule.kt
+++ features/books/src/main/java/com/joniaranguri/platzi/android/books/list/data/local/di/LocalModule.kt
@@ -9,27 +9,19 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Named
 import javax.inject.Singleton
 
 
 @Module
 @InstallIn(SingletonComponent::class)
 class LocalModule {
-    @Provides
-    @Singleton
-    @Named(value = DB_NAME)
-    fun provideDatabaseName(): String {
-        return BOOKS_DB_NAME
-    }
 
     @Provides
     @Singleton
     fun provideDatabase(
-        @Named(value = DB_NAME) dbname: String,
         @ApplicationContext context: Context
     ): BooksDatabase {
-        return Room.databaseBuilder(context, BooksDatabase::class.java, dbname)
+        return Room.databaseBuilder(context, BooksDatabase::class.java, BOOKS_DB_NAME)
             .fallbackToDestructiveMigration()
             .build()
     }
@@ -41,7 +33,6 @@ class LocalModule {
     }
 
     companion object {
-        private const val DB_NAME = "db_name"
         private const val BOOKS_DB_NAME = "books_db"
     }
 }
--- features/books/src/main/java/com/joniaranguri/platzi/android/books/list/data/remote/di/RemoteModule.kt
+++ features/books/src/main/java/com/joniaranguri/platzi/android/books/list/data/remote/di/RemoteModule.kt
@@ -1,24 +1,14 @@
 package com.joniaranguri.platzi.android.books.list.data.remote.di
 
-import android.content.Context
-import com.chuckerteam.chucker.api.ChuckerInterceptor
 import com.joniaranguri.platzi.android.books.list.data.remote.api.BooksApi
-import com.joniaranguri.platzi.android.core.base.app.NetworkConfig
-import com.joniaranguri.platzi.android.core.network.createChuckInterceptor
-import com.joniaranguri.platzi.android.core.network.createHttpLoggingInterceptor
-import com.joniaranguri.platzi.android.core.network.createHttpRequestInterceptor
-import com.joniaranguri.platzi.android.core.network.createMoshi
-import com.joniaranguri.platzi.android.core.network.createOkHttpClient
+import com.joniaranguri.platzi.android.core.data.remote.di.RemoteModule.Companion.BASE_URL
 import com.joniaranguri.platzi.android.core.network.createRetrofitWithMoshi
-import com.joniaranguri.platzi.android.core.network.interceptor.HttpRequestInterceptor
 import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import javax.inject.Named
 import javax.inject.Singleton
 
@@ -26,57 +16,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 class RemoteModule {
-
-    @Provides
-    @Singleton
-    @Named(value = BASE_URL)
-    fun provideBaseUrl(networkConfig: NetworkConfig): String {
-        return networkConfig.baseUrl()
-    }
-
-    @Provides
-    @Singleton
-    fun provideMoshi(): Moshi {
-        return createMoshi()
-    }
-
-    @Provides
-    @Singleton
-    fun provideHttpLoggingInterceptor(networkConfig: NetworkConfig): HttpLoggingInterceptor {
-        return createHttpLoggingInterceptor(isDev = networkConfig.isDev())
-    }
-
-    @Provides
-    @Singleton
-    fun provideHttpRequestInterceptor(): HttpRequestInterceptor {
-        return createHttpRequestInterceptor()
-    }
-
-    @Provides
-    @Singleton
-    fun provideChuckInterceptor(@ApplicationContext context: Context): ChuckerInterceptor {
-        return createChuckInterceptor(context)
-    }
-
-    @Provides
-    @Singleton
-    fun provideOkHttpClient(
-        @ApplicationContext context: Context,
-        httpLoggingInterceptor: HttpLoggingInterceptor,
-        chuckerInterceptor: ChuckerInterceptor,
-        httpRequestInterceptor: HttpRequestInterceptor
-    ): OkHttpClient {
-        return createOkHttpClient(
-            isCache = true,
-            interceptors = arrayOf(
-                httpLoggingInterceptor,
-                chuckerInterceptor,
-                httpRequestInterceptor
-            ),
-            context = context
-        )
-    }
-
     @Provides
     @Singleton
     fun provideBooksApi(
@@ -91,8 +30,4 @@ class RemoteModule {
         )
     }
 
-    companion object {
-        private const val BASE_URL = "base_url"
-
-    }
 }
