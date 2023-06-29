2db713b1d9ec89b1589e877ae5682f808126f8cf
Jonathan Aranguri <arangurim@gmail.com>
2023-06-29 07:39:47 -0300

    feat: Changing app icons and little fix in app.gradle (#9)

--- app/src/main/kotlin/com/joniaranguri/platzi/android/challenge/navigation/MainActivity.kt
+++ app/src/main/kotlin/com/joniaranguri/platzi/android/challenge/navigation/MainActivity.kt
@@ -3,12 +3,14 @@ package com.joniaranguri.platzi.android.challenge.navigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             RootView()
         }
