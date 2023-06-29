cbf26b7d8a2fd34a4fe80d1a1604099592d13a8f
Jonathan Aranguri <arangurim@gmail.com>
2023-06-29 02:31:26 -0300

    feat: Adding little animation to ErrorView

--- common/ui/src/main/java/com/joniaranguri/platzi/android/ui/widgets/BaseErrorView.kt
+++ common/ui/src/main/java/com/joniaranguri/platzi/android/ui/widgets/BaseErrorView.kt
@@ -1,5 +1,9 @@
 package com.joniaranguri.platzi.android.ui.widgets
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -20,30 +24,36 @@ import com.joniaranguri.platzi.android.ui.ext.defaultPadding
 
 @Composable
 fun BaseErrorView(e: Throwable, action: () -> Unit) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .defaultContentPadding(),
-        verticalArrangement = Arrangement.spacedBy(
-            defaultPadding(),
-            alignment = Alignment.CenterVertically
-        ),
-        horizontalAlignment = Alignment.CenterHorizontally
+    AnimatedVisibility(
+        visible = true,
+        enter = fadeIn(animationSpec = tween(2000)),
+        exit = fadeOut(animationSpec = tween(2000))
     ) {
-        TitleLarge(text = "!Ups!")
-        AsyncImage(
-            model = ImageRequest.Builder(LocalContext.current)
-                .data(R.drawable.error_image)
-                .crossfade(500).build(),
-            contentDescription = "Error image illustration",
-            contentScale = ContentScale.Inside,
-            modifier = Modifier.fillMaxWidth(),
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .defaultContentPadding(),
+            verticalArrangement = Arrangement.spacedBy(
+                defaultPadding(),
+                alignment = Alignment.CenterVertically
+            ),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TitleLarge(text = "!Ups!")
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(R.drawable.error_image)
+                    .crossfade(500).build(),
+                contentDescription = "Error image illustration",
+                contentScale = ContentScale.Inside,
+                modifier = Modifier.fillMaxWidth(),
 
-            )
-        TitleLarge(text = "Something went wrong")
-        Text(text = e.message.orEmpty())
-        Button(onClick = action) {
-            Text("Retry")
+                )
+            TitleLarge(text = "Something went wrong")
+            Text(text = e.message.orEmpty())
+            Button(onClick = action) {
+                Text("Retry")
+            }
         }
     }
 }
